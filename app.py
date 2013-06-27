ce4f15df3a07a14d73c34c22baea2905546d6125
Nicholas Tomko <nick.tomko@gmail.com>
2013-06-27 16:37:12 -0700

    change app.py url

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     #port = int(os.environ.get('PORT', 5000))
     #app.debug = True
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    app.run(host='0.0.0.0', port=80)
+    #app.run()
